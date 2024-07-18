6b2a3940e0b2cea388517b594e3a69cf599f97d6
Hunaisp <hunaispc8@gmail.com>
2024-07-18 21:51:25 +0530

    so

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+//import the express module
+//* express is a package in node
+const express = require('express');
+
+// now going to create a express server
+//Steps
+
+// 1- Define the port number the server will listen on :-
+const PORT=3000;
+// 2- create an instance for express server :-
+const app=express();
+// 3- start the server and listen the specified port :-
+app.listen(PORT,"0.0.0.0",function(){
+    //print the server log number
+    console.log(`server is running on ${PORT}`);
+})
